6cdc2423b3e7aaac622af565f5ad3f713dd53505
Justin Bruce Van Horne <justinvh@gmail.com>
2011-01-03 21:25:44 -0700

    Adds new cmake option and fixed type_conversion.hpp
    - Adds HAT_USE_OPENAL (default: TRUE)

--- engine/include/hat/v8/type_conversion.hpp
+++ engine/include/hat/v8/type_conversion.hpp
@@ -8,7 +8,7 @@
 
 namespace easy {
 template <class Type>
-bool smart_get(const v8::Handle<v8::Object>& from, const char* key, Type* to);
+bool smart_convert(const v8::Handle<v8::Value>& from, Type* to);
 
 template <class Type>
 bool smart_convert(const Type& from, v8::Handle<v8::Value>* to);
@@ -20,8 +20,6 @@ bool convert(const v8::Handle<v8::Value>& from, bool* to);
 bool convert(const bool& from, v8::Handle<v8::Value>* to);
 bool convert(const v8::Handle<v8::Value>& from, int32_t* to);
 bool convert(const int32_t& from, v8::Handle<v8::Value>* to);
-bool convert(const v8::Handle<v8::Value>& from, int64_t* to);
-bool convert(const int64_t& from, v8::Handle<v8::Value>* to);
 bool convert(const v8::Handle<v8::Value>& from, double* to);
 bool convert(const double& from, v8::Handle<v8::Value>* to);
 bool convert(const v8::Handle<v8::Value>& from, float* to);
@@ -38,7 +36,7 @@ bool convert(const v8::Handle<v8::Value>& from, Insert_container* to)
     std::insert_iterator<Insert_container> insert_iter(*to, to->begin());
     for (uint32_t i = 0; i < array_val->Length(); i++) {
       typename Insert_container::value_type local;
-      v8::Handle<v8::Value> val = array_val->Get(i);
+      const v8::Handle<v8::Value> val = array_val->Get(i);
       if (!smart_convert(val, &local)) {
         return false;
       }
